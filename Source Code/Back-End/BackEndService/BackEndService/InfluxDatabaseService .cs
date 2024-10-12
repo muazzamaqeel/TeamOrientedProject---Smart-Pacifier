8c91afa6f6fbd3d330ad69bd60f1f04a24e8febe
muazzamaqeel <aqeemu01@thu.de>
2024-10-12 17:00:25 +0200

    Update 1

---
+++ Source Code/Back-End/BackEndService/BackEndService/InfluxDatabaseService .cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+using Interface_NETInterop;
+
+namespace BackEndService
+{
+    public class InfluxDatabaseService : IDatabaseService
+    {
+        private readonly InfluxDBClient _client;
+        private readonly string _bucket = "SmartPacifier-Bucket1"; // Match with the bucket name in InfluxDB
+        private readonly string _org = "thu-de";  // Match with your organization name
+
+        public InfluxDatabaseService(string url, string token)
+        {
+            // Correct initialization of the InfluxDB client
+            _client = new InfluxDBClient(url, token);
+        }
+
+        public void WriteData(string measurement, Dictionary<string, object> fields, Dictionary<string, string> tags)
+        {
+            // Create a new PointData for the measurement
+            var point = PointData.Measurement(measurement)
+                                 .Timestamp(DateTime.UtcNow, WritePrecision.Ns);
+
+            // Add each tag to the point
+            foreach (var tag in tags)
+            {
+                point = point.Tag(tag.Key, tag.Value);
+            }
+
+            // Add each field to the point
+            foreach (var field in fields)
+            {
+                point = point.Field(field.Key, field.Value);
+            }
+
+            using (var writeApi = _client.GetWriteApi())
+            {
+                // Write the point to InfluxDB using the correct arguments
+                writeApi.WritePoint(point, _bucket, _org);
+            }
+
+            Console.WriteLine("Data written to InfluxDB.");
+        }
+
+        public List<string> ReadData(string query)
+        {
+            var queryApi = _client.GetQueryApi();
+            var fluxQuery = queryApi.QueryAsync(query, _org);
+
+            // Convert the query result to a list of strings
+            return fluxQuery?.Result.Select(x => x.ToString()).ToList() ?? new List<string>();
+        }
+    }
+}
---
+++ Source Code/Interface/Interface_NETInterop/Interface_NETInterop/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Interface_NETInterop
+{
+    public interface IDatabaseService
+    {
+        // Method to write data to the database
+        void WriteData(string measurement, Dictionary<string, object> fields, Dictionary<string, string> tags);
+
+        // Method to read data from the database
+        List<string> ReadData(string query);
+    }
+}
