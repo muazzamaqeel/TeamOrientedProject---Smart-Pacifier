fcd4d042c82356e721d173e7b0bd105a7341a956
muazzamaqeel <aqeemu01@thu.de>
2024-10-19 14:17:21 +0200

    Templates+MainSuite

--- Testing_Integration_Environment/SmartPacifier - UITests/SmartPacifier - UITests/Main_TestSuite.cs
+++ Testing_Integration_Environment/SmartPacifier - UITests/SmartPacifier - UITests/Main_TestSuite.cs
@@ -1,12 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using FlaUI.Core;
+using FlaUI.UIA3;
+using Xunit;
+using SmartPacifier___UITests.Tests_FrontEnd.Tests_Tabs.Tests_SettingTab;
 
 namespace SmartPacifier___UITests
 {
     public class Main_TestSuite
     {
+        private Application? app;
+
+        /// <summary>
+        /// This function is used to launch the application for all UI Tests.
+        /// </summary>
+        /// <returns></returns>
+        private Application LaunchApplication()
+        {
+            string applicationPath = @"C:\programming\TeamOrientedProject---Smart-Pacifier\Source Code\Front-End\UI (WPF)\Smart Pacifier - Tool\Smart Pacifier - Tool\bin\Debug\net8.0-windows\SmartPacifier.UI (WPF).exe";
+            var processStartInfo = new ProcessStartInfo(applicationPath)
+            {
+                WorkingDirectory = @"C:\programming\TeamOrientedProject---Smart-Pacifier\Source Code\Front-End\UI (WPF)\Smart Pacifier - Tool\Smart Pacifier - Tool\bin\Debug\net8.0-windows\"
+            };
+
+            app = Application.Launch(processStartInfo);
+            return app;
+        }
+
+        /// <summary>
+        /// Manually run the CheckButtonsExistenceInSettingsTab test from the UITests_Settings class.
+        /// </summary>
+        [Fact]
+        public void RunSettingsTabTests()
+        {
+            // Launch the application once here
+            app = LaunchApplication();
+
+            try
+            {
+                // Pass the launched application to UITests_Settings
+                var settingsTests = new UITests_Settings(app);
+
+                // Run the settings tab tests
+                settingsTests.CheckButtonsExistenceInSettingsTab();
+            }
+            finally
+            {
+                // Close the application after running tests
+                app.Close();
+            }
+        }
     }
 }
--- Testing_Integration_Environment/SmartPacifier - UITests/SmartPacifier - UITests/Tests-FrontEnd/Tests-Tabs/Tests-SettingTab/UITests_Settings.cs
+++ Testing_Integration_Environment/SmartPacifier - UITests/SmartPacifier - UITests/Tests-FrontEnd/Tests-Tabs/Tests-SettingTab/UITests_Settings.cs
@@ -1,34 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FlaUI.Core;
+﻿using FlaUI.Core;
 using FlaUI.UIA3;
-using FlaUI.Core.AutomationElements;
-using FlaUI.Core.Definitions;
 using Xunit;
+using SmartPacifier___UITests.Templates; // Importing the Buttons class
 
 namespace SmartPacifier___UITests.Tests_FrontEnd.Tests_Tabs.Tests_SettingTab
 {
     public class UITests_Settings
     {
+        public readonly Application app;
 
-        /// <summary>
-        /// This function is used in this class to basically be used over and over again to launch the application for all the UI Tests that will be take from other classes.
-        /// </summary>
-        /// <returns></returns>
-        private Application LaunchApplication()
+        // Constructor to pass the launched application
+        public UITests_Settings(Application app)
         {
-            string applicationPath = @"C:\programming\TeamOrientedProject---Smart-Pacifier\Source Code\Front-End\UI (WPF)\Smart Pacifier - Tool\Smart Pacifier - Tool\bin\Debug\net8.0-windows\SmartPacifier.UI (WPF).exe";
-            var processStartInfo = new ProcessStartInfo(applicationPath)
-            {
-                WorkingDirectory = @"C:\programming\TeamOrientedProject---Smart-Pacifier\Source Code\Front-End\UI (WPF)\Smart Pacifier - Tool\Smart Pacifier - Tool\bin\Debug\net8.0-windows\"
-            };
-
-            return Application.Launch(processStartInfo);
+            this.app = app;
         }
 
+        public void CheckButtonsExistenceInSettingsTab()
+        {
+            try
+            {
+                using (var automation = new UIA3Automation())
+                {
+                    Thread.Sleep(3000); // Ensure the UI is loaded properly
+                    var mainWindow = app.GetMainWindow(automation);
+
+                    var buttonsHelper = new Buttons(); // Instance of the Buttons class
+
+                    // Check for the existence of buttons in the Settings tab
+                    var switchModeButton = buttonsHelper.FindButtonByName(mainWindow, "SwitchModeButton");
+                    var themeButton = buttonsHelper.FindButtonByName(mainWindow, "ThemeButton");
+                    var userModeButton = buttonsHelper.FindButtonByName(mainWindow, "UserModeButton");
+                    var developerModeButton = buttonsHelper.FindButtonByName(mainWindow, "DeveloperModeButton");
+
+                    // Assert that each button exists
+                    Assert.NotNull(switchModeButton);
+                    Assert.NotNull(themeButton);
+                    Assert.NotNull(userModeButton);
+                    Assert.NotNull(developerModeButton);
+                }
+            }
+            finally
+            {
+                // Note: Don't close the app here; it will be closed by Main_TestSuite
+            }
+        }
     }
 }
