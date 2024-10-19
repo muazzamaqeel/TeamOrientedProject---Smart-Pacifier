df5b1a0c918322b0bfa53a652929caef442a1249
muazzamaqeel <aqeemu01@thu.de>
2024-10-19 13:44:17 +0200

    Create UITests_Settings.cs

---
+++ Testing_Integration_Environment/SmartPacifier - UITests/SmartPacifier - UITests/Tests-FrontEnd/Tests-Tabs/Tests-SettingTab/UITests_Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPacifier___UITests.Tests_FrontEnd.Tests_Tabs.Tests_SettingTab
+{
+    internal class UITests_Settings
+    {
+    }
+}
