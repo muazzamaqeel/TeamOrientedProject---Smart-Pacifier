ed95c2ebd31e8622471a712676632226c7bfe6cf
muazzamaqeel <aqeemu01@thu.de>
2024-10-19 13:51:21 +0200

    Temp

--- Testing_Integration_Environment/SmartPacifier - UITests/SmartPacifier - UITests/Tests-FrontEnd/Tests-Tabs/Tests-SettingTab/UITests_Settings.cs
+++ Testing_Integration_Environment/SmartPacifier - UITests/SmartPacifier - UITests/Tests-FrontEnd/Tests-Tabs/Tests-SettingTab/UITests_Settings.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlaUI.Core;
+using FlaUI.UIA3;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
+using Xunit;
 
 namespace SmartPacifier___UITests.Tests_FrontEnd.Tests_Tabs.Tests_SettingTab
 {
-    internal class UITests_Settings
+    public class UITests_Settings
     {
+
+        /// <summary>
+        /// This function is used in this class to basically be used over and over again to launch the application for all the UI Tests that will be take from other classes.
+        /// </summary>
+        /// <returns></returns>
+        private Application LaunchApplication()
+        {
+            string applicationPath = @"C:\programming\TeamOrientedProject---Smart-Pacifier\Source Code\Front-End\UI (WPF)\Smart Pacifier - Tool\Smart Pacifier - Tool\bin\Debug\net8.0-windows\SmartPacifier.UI (WPF).exe";
+            var processStartInfo = new ProcessStartInfo(applicationPath)
+            {
+                WorkingDirectory = @"C:\programming\TeamOrientedProject---Smart-Pacifier\Source Code\Front-End\UI (WPF)\Smart Pacifier - Tool\Smart Pacifier - Tool\bin\Debug\net8.0-windows\"
+            };
+
+            return Application.Launch(processStartInfo);
+        }
+
     }
 }
