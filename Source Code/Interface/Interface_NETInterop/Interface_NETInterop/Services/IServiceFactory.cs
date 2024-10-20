e4b6eae147a89a6f2619a5106bbda43125eff5af
muazzamaqeel <aqeemu01@thu.de>
2024-10-20 23:19:00 +0200

    Fix

--- Source Code/Interface/Interface_NETInterop/Interface_NETInterop/Services/IServiceFactory.cs
+++ Source Code/Interface/Interface_NETInterop/Interface_NETInterop/Services/IServiceFactory.cs
@@ -6,6 +6,7 @@
     /// The IServiceFactory interface is used to create the services that are needed by the backend
     /// </summary>
     public interface IServiceFactory
+
     {
         IAlgorithmLayer CreateAlgorithmService();
         IDatabaseService CreateDatabaseService(string url, string token);
