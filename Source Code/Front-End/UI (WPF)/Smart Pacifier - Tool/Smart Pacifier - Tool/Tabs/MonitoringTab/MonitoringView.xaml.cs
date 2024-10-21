7191e18174a6c4ed2fef4726089bdd594bed34d2
VincentStriegel <strivi01@thu.de>
2024-10-21 15:54:56 +0200

    Add table component and create Raw Data view

--- Source Code/Front-End/UI (WPF)/Smart Pacifier - Tool/Smart Pacifier - Tool/Tabs/MonitoringTab/MonitoringView.xaml.cs
+++ Source Code/Front-End/UI (WPF)/Smart Pacifier - Tool/Smart Pacifier - Tool/Tabs/MonitoringTab/MonitoringView.xaml.cs
@@ -1,15 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Smart_Pacifier___Tool.Tabs.MonitoringTab
 {
-    /// <summary>
-    /// Interaction logic for MonitoringView.xaml
-    /// </summary>
     public partial class MonitoringView : UserControl
     {
         public MonitoringView()
         {
             InitializeComponent();
+        }
+
+        private void OpenRawDataView_Click(object sender, RoutedEventArgs e)
+        {
+            // Define the properties to pass
+            string pacifierName = "Pacifier 01";
+
+            // Create an instance of RawDataView with the properties and a reference to this view
+            var rawDataView = new RawDataView(pacifierName, this);
+
+            // Replace the current view with RawDataView
+            var parent = this.Parent as ContentControl;
+            if (parent != null)
+            {
+                parent.Content = rawDataView;
+            }
         }
     }
 }
