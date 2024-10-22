950176dac29e4a70e5d618d61f38f9c541ed1bb9
Simon Engel <engesi02@thu.de>
2024-10-22 14:16:10 +0200

    Test

---
+++ Source Code/Temp/Program.cs
@@ -0,0 +1,10 @@
+﻿using SmartPacifier.BackEnd.IOTProtocols;
+
+class TestClass
+{
+    static async Task Main(string[] args)
+    {
+	Broker broker = Broker.Instance;
+	await broker.ConnectBroker();
+    }
+}
