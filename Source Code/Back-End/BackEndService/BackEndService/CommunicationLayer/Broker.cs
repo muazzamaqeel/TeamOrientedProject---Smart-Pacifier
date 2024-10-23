2bedeee44f90cc4d40b555f42755e9f151a06807
Simon Engel <engesi02@thu.de>
2024-10-23 19:55:59 +0200

    Test

--- Source Code/Back-End/BackEndService/BackEndService/CommunicationLayer/Broker.cs
+++ Source Code/Back-End/BackEndService/BackEndService/CommunicationLayer/Broker.cs
@@ -13,42 +13,42 @@ namespace SmartPacifier.BackEnd.IOTProtocols
 {
     public class Broker
     {
-        private static Broker _brokerInstance;
+        private readonly string BROKER_ADDRESS = "localhost";
+        private readonly int BROKER_PORT = 1883;
+
+        private static Broker? _brokerInstance;
         private static readonly object _lock = new object();
 
-	private IMqttClient _mqttClient;
-	//private readonly MQTTnet.Server.MqttClient _mqttServer;
-	
-        private readonly string _brokerAddress = "localhost";
-        private readonly int _brokerPort = 1883;
+        private IMqttClient? _mqttClient;
+        //private readonly MQTTnet.Server.MqttClient _mqttServer;
 
         private Process _brokerProcess;
-	private Task _brokerTask;
-	private bool _isBrokerRunning = false;
+        private Task _brokerTask;
+        private bool _isBrokerRunning = false;
 
-	/// <summary>
-	/// Constructor for the Broker class
-	/// </summary>
+        /// <summary>
+        /// Constructor for the Broker class
+        /// </summary>
         private Broker()
         {
-	    var factory = new MqttFactory();
-	    _mqttClient = factory.CreateMqttClient();
-	    StartBroker();
-	    //ConnectBroker();
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+            StartBroker();
+            //ConnectBroker();
         }
-	///<summary>
-	/// Destructor to clean up the Broker
-	///</summary>
+        ///<summary>
+        /// Destructor to clean up the Broker
+        ///</summary>
         ~Broker()
         {
-	    StopBroker();
+            StopBroker();
         }
 
-	/// <summary>
-       	/// Gettingan Instance of the Broker. If there is no intance
-       	/// yet, it will create one. This is threadsafe for
-       	/// multithreading
-	/// </summary>
+        /// <summary>
+	/// Gettingan Instance of the Broker. If there is no intance
+        /// yet, it will create one. This is threadsafe for
+        /// multithreading
+        /// </summary>
         public static Broker Instance
         {
             get
@@ -64,59 +64,35 @@ namespace SmartPacifier.BackEnd.IOTProtocols
             }
         }
 
-	/// <summary>
-	/// Starting Mosquitto as a child process
-	/// </summary>
-	/// <returns>True if process started sucessfully, false otherwise</returns>
-        public void StartBroker()
-	{
-	    _brokerTask = Task.Run(() =>
-	    {
-		_isBrokerRunning = true;
-		var processStartInfo = new ProcessStartInfo();
-		processStartInfo.FileName = "mosquitto"; // path to Mosquitto executable
-		processStartInfo.Arguments = ""; //Mosquitto command-line arguments
-		processStartInfo.UseShellExecute = false;
-		processStartInfo.RedirectStandardOutput = true;
-		processStartInfo.RedirectStandardError = true;
-
-		_brokerProcess = Process.Start(processStartInfo);
-	    
-		if (_brokerProcess != null)
-		{
-		    // Optionally, you can read the output from the Mosquitto process
-		    var output = _brokerProcess.StandardOutput.ReadToEnd();
-		    var error = _brokerProcess.StandardError.ReadToEnd();
-		    Console.WriteLine("Mosquitto output:\n" + output);
-		    Console.WriteLine("Mosquitto errors:\n" + error);
-		    // return true;
-		}
-		else
-		{
-		    Console.WriteLine("Failed to start Mosquitto process.");
-		    _isBrokerRunning = false;
-		    //return false;
-		}
-	    });
-	    //return false;
-	    MonitorTaskStatus();
-	}
-	/// <summary>
-        /// Prints the task status periodically.
+        /// <summary>
+        /// Starting Mosquitto as a child process
         /// </summary>
-        public void MonitorTaskStatus()
+        /// <returns>True if process started sucessfully, false otherwise</returns>
+        public void StartBroker()
         {
-            Task.Run(() =>
-            {
-                while (_isBrokerRunning)
-                {
-                    Console.WriteLine("Broker process is still running...");
-                    Thread.Sleep(1000); // Wait 1 second before checking again
-                }
+	    var processStartInfo = new ProcessStartInfo();
+	    processStartInfo.FileName = "mosquitto"; // path to Mosquitto executable
+	    processStartInfo.Arguments = "-v"; //Mosquitto command-line arguments
+	    processStartInfo.UseShellExecute = false;
+	    processStartInfo.RedirectStandardOutput = true;
+	    processStartInfo.RedirectStandardError = true;
 
-                Console.WriteLine("Broker process has stopped.");
-            });
-        }
+	    _brokerProcess = Process.Start(processStartInfo);
+
+	    if (_brokerProcess != null)
+	    {
+		// Optionally, you can read the output from the Mosquitto process
+		var output = _brokerProcess.StandardOutput.ReadToEnd();
+		var error = _brokerProcess.StandardError.ReadToEnd();
+		Console.WriteLine("Mosquitto output:\n" + output);
+		Console.WriteLine("Mosquitto errors:\n" + error);
+	    }
+	    else
+	    {
+		Console.WriteLine("Failed to start Mosquitto process.");
+		_isBrokerRunning = false;
+	    }
+        } 
 
         public void StopBroker()
         {
@@ -124,41 +100,42 @@ namespace SmartPacifier.BackEnd.IOTProtocols
         }
 
         public async Task ConnectBroker()
-	{
-	    // Use TCP connection.
-	    var options = new MqttClientOptionsBuilder()
-		.WithTcpServer("broker.hivemq.com", 1883) // Optional port
-		.Build();
-	    
-	    try
-	    {
-		await _mqttClient.ConnectAsync(options);
-	    }
-	    catch (Exception ex)
-	    {
-		Console.WriteLine("Failed to connect to MQTT broker: " + ex.Message);
-		return;
-	    }
-	   Console.WriteLine("Sucessfully Connected to MQTT broker "); 
-	}
-	// public async Task SendMessageToAllSensorNodesAsync(string message)
-	// {
-	//     var topic = "to_all_sensor_nodes"; // Adjust the topic as needed
-	//     var mqttMessage = new MqttApplicationMessageBuilder()
-	// 	.WithTopic(topic)
-	// 	.WithPayload(message)
-	// 	.WithQualityOfService(MqttQualityOfService.AtLeastOnce)
-	// 	.Build();
+        {
+            // Use TCP connection.
+            var options = new MqttClientOptionsBuilder()
+            .WithTcpServer(BROKER_ADDRESS, BROKER_PORT) // Optional port
+            .Build();
 
-	//     try
-	//     {
-	// 	await _mqttClient.PublishAsync(mqttMessage);
-	// 	Console.WriteLine("Message sent to all sensor nodes.");
-	//     }
-	//     catch (Exception ex)
-	//     {
-	// 	Console.WriteLine("Failed to send message to sensor nodes: " + ex.Message);
-	//     }
-	// }
+            try
+            {
+                await _mqttClient.ConnectAsync(options);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect to MQTT broker: " + ex.Message);
+                return;
+            }
+            Console.WriteLine("Sucessfully Connected to MQTT broker ");
+        }
+	
+        // public async Task SendMessageToAllSensorNodesAsync(string message)
+        // {
+        //     var topic = "to_all_sensor_nodes"; // Adjust the topic as needed
+        //     var mqttMessage = new MqttApplicationMessageBuilder()
+        // 	.WithTopic(topic)
+        // 	.WithPayload(message)
+        // 	.WithQualityOfService(MqttQualityOfService.AtLeastOnce)
+        // 	.Build();
+
+        //     try
+        //     {
+        // 	await _mqttClient.PublishAsync(mqttMessage);
+        // 	Console.WriteLine("Message sent to all sensor nodes.");
+        //     }
+        //     catch (Exception ex)
+        //     {
+        // 	Console.WriteLine("Failed to send message to sensor nodes: " + ex.Message);
+        //     }
+        // }
     }
 }
